ebdeaf354ff39c86c951a2be8ea84f5666566a0e
cheng <cheng@hicling.com>
2018-12-19 16:58:54 +0800

    a gulp flow  demo

---
+++ build/gulpfile.init.js
@@ -0,0 +1,16 @@
+
+var gulp = require('gulp');
+var mkdirp = require('mkdirp');
+var Config = require('./gulpfile.config');
+
+//======= gulp init 初始化项目结构 ===============
+
+function  init() {
+    gulp.task('init',function(){
+        var dirs = [Config.html.dir, Config.assets.dir, Config.css.dir, Config.sass.dir, Config.js.dir, Config.img.dir];
+        dirs.forEach(dir =>{
+            mkdirp.sync(dir);
+        })
+    })
+}
+module.exports = init;
